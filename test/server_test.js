da0eed42a648263807ed5aed661400a71e803656
david.bashford <dbashford@bericotechnologies.com>
2015-07-04 17:54:46 -0400

    parallel tests done

--- test/server_test.js
+++ test/server_test.js
@@ -211,40 +211,54 @@ describe("pre", function() {
     });
   });
 
-  var buildPreSerialTestConfig = function(pre) {
-    return new RouteBuilder()
+  var buildPreSerialTestConfig = function(pre1, pre2, pre3) {
+    var config = new RouteBuilder()
       .url("/api/foo")
-      .get()
-      .preSerial(pre)
-      .handler(handler)
+      .get();
+
+    if (pre3) {
+      config = config.preSerial(pre1, pre2, pre3)
+    } else if (pre2) {
+      config = config.preSerial(pre1, pre2)
+    } else {
+      config = config.preSerial(pre1)
+    }
+
+    return config.handler(handler)
       .build();
   };
 
-  describe("serial", function() {
+  var suite = function(buildPre) {
     it("1 argument, array", function(done) {
-      var config = buildPreSerialTestConfig([{assign:"testVar", method:preFunc}])
+      var config = buildPre([{assign:"testVar", method:preFunc}]);
       test(done, config, true);
     });
 
     it("1 argument, function", function(done) {
-      var config = buildPreSerialTestConfig(preFunc);
+      var config = buildPre(preFunc);
       test(done, config, false);
     });
 
     it("1 argument, object", function(done) {
-      var config = buildPreSerialTestConfig({assign:"testVar", method:preFunc})
+      var config = buildPre({assign:"testVar", method:preFunc})
+      test(done, config, true);
+    });
+
+    it("1 argument, string", function(done){
+      called = true;
+      var config = buildPre("testVar(query.bar)")
       test(done, config, true);
     });
 
     it("2 arguments", function(done) {
-      var config = new RouteBuilder()
-        .url("/api/foo")
-        .get()
-        .preSerial("testVar", preFunc)
-        .handler(handler)
-        .build();
+      var config = buildPre("testVar", preFunc);
       test(done, config, true);
     });
+  };
+
+  describe("serial", function() {
+
+    suite(buildPreSerialTestConfig);
 
     it("3 arguments", function(done) {
       var preFunc = function(req, reply) {
@@ -258,17 +272,6 @@ describe("pre", function() {
         .handler(handler)
         .build();
       test(done, config, true, '{"statusCode":500,"error":"Internal Server Error","message":"An internal server error occurred"}');
-    });
-
-    it("1 argument, string", function(done){
-      called = true;
-      var config = new RouteBuilder()
-        .url("/api/foo")
-        .get()
-        .preSerial("testVar(query.bar)")
-        .handler(handler)
-        .build();
-      test(done, config, true);
     });
 
     it("2 serials", function(done) {
@@ -286,6 +289,83 @@ describe("pre", function() {
 
   });
 
-  //describe("parallel");
+  var buildPreParalellTestConfig = function(pre1, pre2, pre3) {
+    var config = new RouteBuilder()
+      .url("/api/foo")
+      .get();
+
+    if (pre3) {
+      config = config.preParallel([pre1, pre2, pre3])
+    } else if (pre2) {
+      config = config.preParallel([pre1, pre2])
+    } else {
+      var p = Array.isArray(pre1) ? pre1 : [pre1];
+      config = config.preParallel(p)
+    }
+
+    return config.handler(handler)
+      .build();
+  };
+
+  describe("parallel", function() {
+    suite(buildPreParalellTestConfig);
+
+    it("3 arguments", function(done) {
+      var preFunc = function(req, reply) {
+        called = true;
+        throw new Error("foo-bar");
+      };
+      var config = new RouteBuilder()
+        .url("/api/foo")
+        .get()
+        .preParallel(["testVar", preFunc, "ignore"])
+        .handler(handler)
+        .build();
+      test(done, config, true, '{"statusCode":500,"error":"Internal Server Error","message":"An internal server error occurred"}');
+    });
+
+    it("2 parallels", function(done) {
+      var config = new RouteBuilder()
+        .url("/api/foo")
+        .get()
+        .preParallel(["testVar", preFunc])
+        .preParallel(["testVar", function(request, reply) {
+          reply(request.pre.testVar + "-baz");
+        }])
+        .handler(handler)
+        .build();
+      test(done, config, true, "foo-bar-baz");
+    });
+
+    it("1 parallel, 2 pres", function(done) {
+
+      var preFunc1 = function(request, reply) {
+        reply("fooooo");
+      };
+
+      var preFunc2 = function(request, reply) {
+        reply("barrrr");
+      };
+
+      var config = new RouteBuilder()
+        .url("/api/foo")
+        .get()
+        .preParallel(["testVar1", preFunc1], ["testVar2", preFunc2])
+        .handler(function(request, reply){
+          reply(request.pre.testVar1 + "-" + request.pre.testVar2);
+        })
+        .build();
+
+      new TestServer(config, done).andTest(function(request, stop) {
+        request
+          .get("/api/foo")
+          .end(function(err, res) {
+            expect(res.text).to.eql("fooooo-barrrr");
+            stop();
+          });
+      });
+
+    });
+  });
 })
 
